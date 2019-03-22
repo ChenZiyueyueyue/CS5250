c417de4ee12bcc1d1790fb8c7435886cd85e224c
Chen Ziyue <chenziyue0310@gmail.com>
2019-03-22 13:02:01 +0800

    Update OBCDeviceModule.c
    Add read and write functions

--- OBCDeviceModule.c
+++ OBCDeviceModule.c
@@ -28,3 +28,42 @@ struct file_operations onebyte_fops = {
 };
 
 char *onebyte_data = NULL;
+
+int onebyte_open(struct inode *inode, struct file *filep)
+{
+	return 0; // always successful
+}
+int onebyte_release(struct inode *inode, struct file *filep)
+{
+	return 0; // always successful
+}
+ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
+{
+	int bytes_read = 0;
+	
+	/* Check if the buffer has been written */
+	if(*buf != 0){
+		return 0;	
+	}
+	copy_to_user(buf, onebyte_data, sizeof(char));
+	
+	bytes_read ++;
+	return bytes_read;
+}
+ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
+{
+	int bytes_write = 0;	
+	copy_from_user(onebyte_data, buf, sizeof(char));
+
+	/* Check the length of the bytes that have been written*/
+	if(count > sizeof(char))
+	{
+		printk(KERN_ALERT "No space left on device\n");
+
+		/*Return Linux System Error<28>: No space left on device */
+		return -ENOSPC; 
+	}
+
+	bytes_write ++;
+	return bytes_write;
+}
