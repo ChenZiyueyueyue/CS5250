cfbea83a96e05b148423bbc04eefdc124aa103bc
Chen Ziyue <chenziyue0310@gmail.com>
2019-03-22 14:28:42 +0800

    Update OBCDeviceModule.c
    Set one byte init and exit and make it work

--- OBCDeviceModule.c
+++ OBCDeviceModule.c
@@ -67,3 +67,46 @@ ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t 
 	bytes_write ++;
 	return bytes_write;
 }
+
+static int onebyte_init(void)
+{
+	int result;
+	// register the device
+	result = register_chrdev(MAJOR_NUMBER, "onebyte", &onebyte_fops);
+	if (result < 0) {
+		return result;
+	}
+	// allocate one byte of memory for storage
+	// kmalloc is just like malloc, the second parameter is
+	// the type of memory to be allocated.
+	// To release the memory allocated by kmalloc, use kfree.
+	onebyte_data = kmalloc(sizeof(char), GFP_KERNEL);
+
+	if (!onebyte_data) {
+		onebyte_exit();
+		// cannot allocate memory
+		// return no memory error, negative signify a failure
+		return -ENOMEM;
+	}
+
+	// initialize the value to be X
+	*onebyte_data = 'X';
+
+	printk(KERN_ALERT "This is a onebyte device module\n");
+	return 0;
+}
+
+
+static void onebyte_exit(void)
+{
+	// if the pointer is pointing to something
+	if (onebyte_data) {
+		// free the memory and assign the pointer to NULL
+		kfree(onebyte_data);
+		onebyte_data = NULL;
+	}
+
+	// unregister the device
+	unregister_chrdev(MAJOR_NUMBER, "onebyte");
+	printk(KERN_ALERT "Onebyte device module is unloaded\n");
+}
